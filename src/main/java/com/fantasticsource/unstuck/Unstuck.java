ee42311352409d97614633cd65a3767d6d721796
Kyle Koder <silvertallon@gmail.com>
2019-07-27 20:48:38 -0700

    Initial commit

---
+++ src/main/java/com/fantasticsource/unstuck/Unstuck.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.unstuck;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = Unstuck.MODID, name = Unstuck.NAME, version = Unstuck.VERSION)
+public class Unstuck
+{
+    public static final String MODID = "unstuck";
+    public static final String NAME = "Unstuck";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(Unstuck.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}

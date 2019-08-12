c028b40f7649798bc9a5129edd7d2659e59c167a
Kyle Koder <silvertallon@gmail.com>
2019-08-12 09:34:20 -0700

    Indev build 1.12.2.000a

--- src/main/java/com/fantasticsource/unstuck/Unstuck.java
+++ src/main/java/com/fantasticsource/unstuck/Unstuck.java
@@ -14,7 +14,7 @@ public class Unstuck
 {
     public static final String MODID = "unstuck";
     public static final String NAME = "Unstuck";
-    public static final String VERSION = "1.12.2.000";
+    public static final String VERSION = "1.12.2.000a";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
